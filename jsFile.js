e133dae0f00dc92dce35488185c578a8369bfa85
antoan-97 <142493151+antoan-97@users.noreply.github.com>
2024-01-09 17:58:39 +0200

    Simple Calculator Project
    This is my first project files

---
+++ jsFile.js
@@ -0,0 +1,31 @@
+function appendToDisplay(value) {
+    document.getElementById('display').value += value;
+}
+
+function clearDisplay() {
+    document.getElementById('display').value = '';
+}
+
+function calculateResult() {
+    try {
+        let displayValue = document.getElementById('display').value;
+        // Check if the display contains '%'
+        if (displayValue.includes('%')) {
+            // Replace '%' with '/100*' and evaluate the expression
+            let expression = displayValue.replace(/%/g, '/100*');
+            let result = eval(expression);
+            document.getElementById('display').value = result;
+        } else {
+            // If no '%', perform regular evaluation
+            document.getElementById('display').value = eval(displayValue);
+        }
+    } catch (error) {
+        document.getElementById('display').value = 'Error';
+    }
+}
+
+
+function handleInput() {
+    // Handle input as needed
+    console.log('Input detected:', document.getElementById('display').value);
+}
